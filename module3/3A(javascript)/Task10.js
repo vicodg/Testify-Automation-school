076d4d7ced45fc689d3bdb93e8df13e1667bccbd
victor odogwu <chidivictor56@gmail.com>
2024-03-28 16:43:38 +0000

    add Task 10

---
+++ module3/3A(javascript)/Task10.js
@@ -0,0 +1,8 @@
+function calculateAreaOfRectangle(length, width) {
+    let area = length * width;
+    return area;
+}
+let length = 15;
+let width = 23;
+let area = calculateAreaOfRectangle(length, width);
+console.log("The area of the rectangle is: " + area);
