67be6e4316c9e25c7ffbfbc0695bd78fd1c79426
victor odogwu <chidivictor56@gmail.com>
2024-03-28 14:42:17 +0000

    add Task 9

---
+++ module3/3A(javascript)/Task9.js
@@ -0,0 +1,7 @@
+function calculateAreaOfRectangle(length, width) {
+    let area = length * width;
+
+    console.log("The area of the rectangle is: " + area);
+}
+
+calculateAreaOfRectangle(5, 3); 
