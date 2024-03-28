77a89c04f32fdf83687cc8b2eec11f0cac9fa5a6
victor odogwu <chidivictor56@gmail.com>
2024-03-28 09:45:20 +0000

    add Task 8

---
+++ module3/3A(javascript)/Task8.js
@@ -0,0 +1,10 @@
+// Using a for loop to iterate through numbers from 1 to 20
+for (let number= 1; number <= 20; number=number+1) {
+    // Check if the current number is odd (not divisible by 2)
+    if (number% 2 === 0) {
+        console.log (number+ 'is an even number')
+    }
+    else{
+        console.log (number+ 'is an odd number')
+    }
+}
