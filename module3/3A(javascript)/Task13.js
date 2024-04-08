273013189aa81564a4f13d912993e0a03148c06a
victor odogwu <chidivictor56@gmail.com>
2024-04-08 16:57:10 +0100

    add Task 13

---
+++ module3/3A(javascript)/Task13.js
@@ -0,0 +1,9 @@
+const books = {
+    title: 'The Good Parts of Java',
+    description: 'A book about the good parts of JavaScript.',
+    numberOfPages: 176,
+    author: 'victor odogwu',
+    reading: true
+};
+
+console.log(books.title);
