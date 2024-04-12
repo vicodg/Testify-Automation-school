7d174bea1328914384cc95fe2b5d8c9d651f693a
victor odogwu <chidivictor56@gmail.com>
2024-04-12 10:33:41 +0100

    add JS Code Challange 3

---
+++ module3/3A(javascript)/Js Code Challange /jcc3.js
@@ -0,0 +1,8 @@
+// Loop from 0 to 100
+for (let i = 0; i <= 100; i++) {
+    // Check if the current number is even
+    if (i % 2 === 0) {
+        // Print the even number
+        console.log(i);
+    }
+}
