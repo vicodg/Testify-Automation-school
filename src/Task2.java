4421619a5054e069e726a25abd30277ac60d5fe9
victor odogwu <chidivictor56@gmail.com>
2024-05-16 11:10:04 +0100

    Task project

---
+++ src/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte age =-127;
+        short money =3800;
+        long somuchmoney =8000000;
+        float area = 12365756F;
+        char gender = 'M';
+        boolean flag = true;
+
+
+
+    }
+}
